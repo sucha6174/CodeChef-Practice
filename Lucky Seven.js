412c1d967b96ca85e72f72cef59ef84d8b67b813
KALARI SRISUCHA <23MH1A4426@acoe.edu.in>
2026-01-23 15:33:39 +0530

    Create Lucky Seven.js

---
+++ Lucky Seven.js
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int index = 6;
+        char luckyLetter = s.charAt(index);
+
+
+        System.out.println(luckyLetter);
+
+    }
+}
