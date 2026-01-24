e5b0416e8066da77b9e9815be63bd888e0b5904c
KALARI SRISUCHA <23MH1A4426@acoe.edu.in>
2026-01-24 21:28:28 +0530

    Implement clear day calculation in Clear Day.java
    Fix calculation of clear days by correcting variable name.

---
+++ Clear Day.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int totaldays=7;
+        int clearday=(total-(x+y));
+        System.out.println(clearday);
+
+    }
+}
