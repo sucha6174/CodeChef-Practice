b5ef77489de1040309d2eaaaa7097e279f187c0b
KALARI SRISUCHA <23MH1A4426@acoe.edu.in>
2026-03-04 22:43:42 +0530

    Add Color Festival program to count unique colors
    Implement a program to count unique colors in multiple test cases.

---
+++ Color Festival.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- >0){
+		    int n = sc.nextInt();
+            HashSet<Integer> set = new HashSet<>();
+            for(int i = 0;i<n;i++){
+                int c = sc.nextInt();
+                set.add(c);
+            }
+            System.out.println(set.size());
+		    
+		}
+
+	}
+}
